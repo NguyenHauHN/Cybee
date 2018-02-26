ebe02c149220ddd2226b47be903eb199c5198ed1
haunguyen <nguyenhau1996mta@gmail.com>
2018-02-26 23:27:41 +0700

    footer and customer

--- src/App.js
+++ src/App.js
@@ -39,28 +39,32 @@ class Function extends Component {
         return (
             <div className="Function">
                 <Row style={{maxWidth: 1200}} gutter={24} type="flex" justify="center" className="margin-center">
-                    <Col xs={24} sm={12} md={6} className="display-flex flex-direction align-items-center">
+                    <Col xs={24} sm={12} md={6} style={{margin: '12px 0'}}
+                         className="display-flex flex-direction align-items-center">
                         <img className="Img-function" src={images.search}/>
                         <div className="Name-function">
                             Thu thập thông tin hệ điều hành
                         </div>
                     </Col>
-                    <Col xs={24} sm={12} md={6} className="display-flex flex-direction align-items-center">
+                    <Col xs={24} sm={12} md={6} style={{margin: '12px 0'}}
+                         className="display-flex flex-direction align-items-center">
                         <img className="Img-function" src={images.cycle_time}/>
                         <div className="Name-function">
                             Tự động rà quét lỗ hổng bảo mật
                         </div>
                     </Col>
-                    <Col xs={24} sm={12} md={6} className="display-flex flex-direction align-items-center">
+                    <Col xs={24} sm={12} md={6} style={{margin: '12px 0'}}
+                         className="display-flex flex-direction align-items-center">
                         <img className="Img-function" src={images.chart}/>
                         <div className="Name-function">
                             Hỗ trợ trích xuất báo cáo chi tiết theo lỗ hổng
                         </div>
                     </Col>
-                    <Col xs={24} sm={12} md={6} className="display-flex flex-direction align-items-center">
+                    <Col xs={24} sm={12} md={6} style={{margin: '12px 0'}}
+                         className="display-flex flex-direction align-items-center">
                         <img className="Img-function" src={images.exchange}/>
                         <div className="Name-function">
-                            Tư vấn khắc phúc sự cố an ninh bảo mật
+                            Tư vấn khắc phục sự cố an ninh bảo mật
                         </div>
                     </Col>
                 </Row>
@@ -80,7 +84,7 @@ class SecurityWebsite extends Component {
                     </div>
                 </div>
                 <div className="Content-security">
-                    <Row gutter={24}>
+                    <Row gutter={24} className="No-margin">
                         <Col xs={24} sm={8} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                             <img src={images.web} className="Img-content"/>
                             <div className="Title-content">
@@ -97,7 +101,7 @@ class SecurityWebsite extends Component {
                                 Khắc phục sự cố bảo mật
                             </div>
                             <div className="Description-content">
-                                Bạn không có đội ngủ trong việc xử lý các sự cố liên quan đến vấn đề an ninh bảo mật cho
+                                Bạn không có đội ngũ trong việc xử lý các sự cố liên quan đến vấn đề an ninh bảo mật cho
                                 chính Website của mình.
                             </div>
                         </Col>
@@ -135,7 +139,7 @@ class Solution extends Component {
                         <div className="Description-solution">
 
                             <Row gutter={24} type='flex' style={{margin: '36px 0 26px 0'}}>
-                                <Col xs={24} sm={24} md={12}
+                                <Col xs={{span: 24, order: 2}} md={{span: 12, order: 1}}
                                      style={{
                                          display: 'flex',
                                          flexDirection: 'column',
@@ -159,20 +163,20 @@ class Solution extends Component {
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Cơ sở dữ liệu với trên 200 lỗ hổng website nguy
+                                            hiểm, bao gồm cả OSWAP TOP 10.
                                         </div>
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Bao gồm cả cơ chế quét port(TCP và UDP), phát hiện
+                                            hệ điều hành, phát hiện phiên bản, quét ping và nhiều hơn thế nữa.
                                         </div>
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Liên tục cập nhật các mẫu nhận diện lỗ hổng từ
+                                            những nghiên cứu mới nhất.
                                         </div>
                                     </div>
                                     <div style={{textAlign: 'center', width: '100%'}}>
@@ -180,7 +184,7 @@ class Solution extends Component {
                                     </div>
 
                                 </Col>
-                                <Col xs={24} sm={24} md={12}
+                                <Col xs={{span: 24, order: 1}} md={{span: 12, order: 2}}
                                      style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                                     <img className="Img-solution" src={images.solution_1}/>
                                 </Col>
@@ -204,12 +208,7 @@ class Solution extends Component {
                                         fontWeight: '700',
                                         marginTop: 18,
                                         textAlign: 'left'
-                                    }}>Khắc phục xử lý sự cố</div>
-                                    <div className="Item-solution">
-                                        <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
-                                        </div>
+                                    }}>Khắc phục xử lý sự cố
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
@@ -219,14 +218,20 @@ class Solution extends Component {
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Cơ sở dữ liệu với trên 200 lỗ hổng website nguy
+                                            hiểm, bao gồm cả OSWAP TOP 10.
                                         </div>
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Bao gồm cả cơ chế quét port(TCP và UDP), phát hiện
+                                            hệ điều hành, phát hiện phiên bản, quét ping và nhiều hơn thế nữa.
+                                        </div>
+                                    </div>
+                                    <div className="Item-solution">
+                                        <img src={images.check} className="Img-check"/>
+                                        <div className="Content-item">Liên tục cập nhật các mẫu nhận diện lỗ hổng từ
+                                            những nghiên cứu mới nhất.
                                         </div>
                                     </div>
                                     <div style={{textAlign: 'center', width: '100%'}}>
@@ -236,7 +241,7 @@ class Solution extends Component {
                                 </Col>
                             </Row>
                             <Row gutter={24} type='flex' style={{margin: '36px 0 26px 0'}}>
-                                <Col xs={24} sm={24} md={12}
+                                <Col xs={{span: 24, order: 2}} md={{span: 12, order: 1}}
                                      style={{
                                          display: 'flex',
                                          flexDirection: 'column',
@@ -249,12 +254,7 @@ class Solution extends Component {
                                         fontWeight: '700',
                                         marginTop: 18,
                                         textAlign: 'left'
-                                    }}>Tư vấn đảm bảo an ninh</div>
-                                    <div className="Item-solution">
-                                        <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
-                                        </div>
+                                    }}>Tư vấn đảm bảo an ninh
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
@@ -264,14 +264,20 @@ class Solution extends Component {
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Cơ sở dữ liệu với trên 200 lỗ hổng website nguy
+                                            hiểm, bao gồm cả OSWAP TOP 10.
                                         </div>
                                     </div>
                                     <div className="Item-solution">
                                         <img src={images.check} className="Img-check"/>
-                                        <div className="Content-item">Chủ động phát hiện sớm các lỗ hổng an ninh từ đó
-                                            đưa ra giải pháp phù hợp ngăn chặn.
+                                        <div className="Content-item">Bao gồm cả cơ chế quét port(TCP và UDP), phát hiện
+                                            hệ điều hành, phát hiện phiên bản, quét ping và nhiều hơn thế nữa.
+                                        </div>
+                                    </div>
+                                    <div className="Item-solution">
+                                        <img src={images.check} className="Img-check"/>
+                                        <div className="Content-item">Liên tục cập nhật các mẫu nhận diện lỗ hổng từ
+                                            những nghiên cứu mới nhất.
                                         </div>
                                     </div>
                                     <div style={{textAlign: 'center', width: '100%'}}>
@@ -279,7 +285,7 @@ class Solution extends Component {
                                     </div>
 
                                 </Col>
-                                <Col xs={24} sm={24} md={12}
+                                <Col xs={{span: 24, order: 1}} md={{span: 12, order: 2}}
                                      style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                                     <img className="Img-solution" src={images.solution_3}/>
                                 </Col>
@@ -294,16 +300,66 @@ class Solution extends Component {
     }
 }
 
-class Customer extends Component{
-    render(){
-        return(
-          <div className="Customer">
-              <div className="Title-customer">
+class Customer extends Component {
+    render() {
+        return (
+            <div className="Customer">
+                <div className="Title-customer">
                     <div className="Question">Khách hàng của chúng tôi</div>
                     <div className="Description">Cảm ơn quý Khách Hàng đã đồng hành cùng chúng tôi
                     </div>
                 </div>
-          </div>
+                <Row style={{maxWidth: 1200, margin: '0 auto'}} gutter={24}>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                </Row>
+                <Row style={{maxWidth: 1200, margin: '0 auto'}} gutter={24}>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                    <Col xs={12} sm={6} style={{marginBottom: 24}}>
+                        <img src={images.banner} className="Img-customer"/>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="Footer">
+                <div className="Title-footer" style={{textAlign: 'left'}}>
+                    <img src={images.logo_host} className="Logo-footer"/>
+                    <img src={images.cybee_logo_1} className="Logo-footer"/>
+                </div>
+                <div className="Content-footer">
+                    <div className="Name-company">Công ty cổ phần công nghệ ánh sáng việt</div>
+                    <div className="Brand">trụ sở Hà Nội</div>
+                    <div className="Info-company">Địa chỉ: Số 1, ngõ 73 Hoàng Cầu, Đống Đa, Hà Nội</div>
+                    <div className="Info-company">Điện thoại: 024-4455-3333</div>
+                    <div className="Brand">tp.Hồ chí minh</div>
+                    <div className="Info-company">Địa chỉ: 166/3 Hồ Bá Kiện, P.15, Q.10, Hồ Chí Minh</div>
+                    <div className="Info-company">Điện thoại: 028-4455-3333</div>
+                </div>
+            </div>
         );
     }
 }
@@ -318,6 +374,7 @@ class App extends Component {
                 <SecurityWebsite/>
                 <Solution/>
                 <Customer/>
+                <Footer/>
             </div>
         );
     }
